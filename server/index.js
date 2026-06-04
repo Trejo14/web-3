03a6cdaa62cd0c95ddacbcff451203c2cedd7b7b
Trejo14 <157612512+Trejo14@users.noreply.github.com>
2026-06-03 21:45:59 -0600

    Fix Cannot GET /: serve static files unconditionally

--- server/index.js
+++ server/index.js
@@ -11,12 +11,11 @@ app.use(express.json())
 
 app.use('/api/reviews', reviewsRouter)
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../dist')))
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../dist/index.html'))
-  })
-}
+const distPath = path.join(__dirname, '../dist')
+app.use(express.static(distPath))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(distPath, 'index.html'))
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
